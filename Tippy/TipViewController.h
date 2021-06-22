ae2fa70564773989e02c81373bdc65c4a0f567f3
Marin Hyatt <mhyatt@fb.com>
2021-06-22 16:19:03 -0400

    Added first feature (dismissing keyboard when View is tapped.

---
+++ Tippy/TipViewController.h
@@ -0,0 +1,16 @@
+//
+//  TipViewController.h
+//  Tippy
+//
+//  Created by Marin Hyatt on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TipViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tippy/TipViewController.m
@@ -0,0 +1,36 @@
+//
+//  TipViewController.m
+//  Tippy
+//
+//  Created by Marin Hyatt on 6/22/21.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)onTap:(id)sender {
+    NSLog(@"Hello!");
+    
+    [self.view endEditing:true];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
