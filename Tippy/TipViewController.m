fcdd11f82944fd8333badf8ad6d286c632a238e9
Marin Hyatt <mhyatt@fb.com>
2021-06-22 16:35:45 -0400

    Added functionality: entering bill, calculating tip and total, and handling changes in tip percentage.

--- Tippy/TipViewController.m
+++ Tippy/TipViewController.m
@@ -9,6 +9,11 @@
 
 @interface TipViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipPercentageControl;
+
 @end
 
 @implementation TipViewController
@@ -19,8 +24,22 @@
 }
 - (IBAction)onTap:(id)sender {
     NSLog(@"Hello!");
-    
+    // Dismisses keyboard
     [self.view endEditing:true];
+}
+- (IBAction)updateLabels:(id)sender {
+    //Calculates tip percentage based on which segment was chosen
+    double tipPercentages[] = {0.15, 0.2, 0.25};
+    double tipPercentage = tipPercentages[self.tipPercentageControl.selectedSegmentIndex];
+    
+    //Calculates bill, tip, and total.
+    double bill = [self.billField.text doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    //Sets tip and total with correctly formatted numbers.
+    self.tipLabel.text = [NSString stringWithFormat:@"$%0.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%0.2f", total];;
 }
 
 /*
